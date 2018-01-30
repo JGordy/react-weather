2146c394e069ba37165b78e44daa9ae68e9fbdd2
Joseph Gordy <jgordy2424@gmail.com>
2018-01-30 17:10:02 -0500

    Started the filtering of the forecast data to organize it the way I need to render on screen.

--- src/containers/WeatherDisplay/WeatherDisplay.js
+++ src/containers/WeatherDisplay/WeatherDisplay.js
@@ -2,15 +2,44 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getForecastByCityID } from '../../actions/action';
 import './WeatherDisplay.css';
-import * as Typeicons from 'react-icons/lib/ti';
+// import * as Typeicons from 'react-icons/lib/ti';
 
 class WeatherDisplay extends Component {
-  // constructor(props) {
-  //   super(props)
-  //   this.state = {
-  //     cityForecast: ''
-  //   }
-  // }
+  constructor(props) {
+    super(props)
+    this.state = {
+      cityForecast: {}
+    }
+  }
+
+  filterForecastData = (forecastData) => {
+    let firstDate = forecastData[0].dt_txt.split(" "),
+        secondDate = forecastData[8].dt_txt.split(" "),
+        thirdDate = forecastData[16].dt_txt.split(" "),
+        fourthDate = forecastData[24].dt_txt.split(" "),
+        fifthDate = forecastData[32].dt_txt.split(" "),
+        filteredForecast = {
+          dayOne: [],
+          dayTwo: [],
+          dayThree: [],
+          dayFour: [],
+          dayFive: []
+        }
+    forecastData.map(hour => {
+      if (hour.dt_txt.includes(firstDate[0])) {
+        filteredForecast.dayOne.push(hour);
+      } else if (hour.dt_txt.includes(secondDate[0])) {
+        filteredForecast.dayTwo.push(hour);
+      } else if (hour.dt_txt.includes(thirdDate[0])) {
+        filteredForecast.dayThree.push(hour);
+      } else if (hour.dt_txt.includes(fourthDate[0])) {
+        filteredForecast.dayFour.push(hour);
+      } else if (hour.dt_txt.includes(fifthDate[0])) {
+        filteredForecast.dayFive.push(hour);
+      }
+    });
+    return filteredForecast;
+  }
 
   componentDidMount() {
     this.props.getForecastByCityID(this.props.place.id);
@@ -18,12 +47,14 @@ class WeatherDisplay extends Component {
   }
 
   render() {
-    console.log("PRAWPS----> ", this.props);
+    // console.log("PRAWPS----> ", this.props);
     let dailyWeather = this.props.dailyWeather,
         cityForecast = this.props.cityForecast,
         city = this.props.place,
         hourlyWeather;
-    if (cityForecast) {
+    if (cityForecast && dailyWeather) {
+       let filteredForecast = this.filterForecastData(cityForecast.list);
+       console.log("filteredForecast----> ", filteredForecast);
       hourlyWeather = cityForecast.list.map((hour, index) => {
         // console.log("HOUR---->",hour);
         return <div className='hours' key={index}>
