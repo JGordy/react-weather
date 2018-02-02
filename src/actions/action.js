9afcd94785d86ea0447bb465a8c45c1711c0231f
Joseph Gordy <jgordy2424@gmail.com>
2018-02-02 10:24:30 -0500

    Added services file to move the api calls out of the action itself

--- src/actions/action.js
+++ src/actions/action.js
@@ -1,3 +1,4 @@
+import services from '../services/service';
 export const SET_DATA = "SET_DATA";
 export const SET_CITY = "SET_CITY";
 
@@ -18,25 +19,19 @@ export function setCity(payload) {
 // calling the api for the group of cities to get weather data
 export const getWeatherData = (payload) => {
   return(dispatch, getState) => {
-    fetch(`http://api.openweathermap.org/data/2.5/group?id=${payload}&units=imperial&APPID=23aa34fce77d5b7782d01b0917e6d2b6`)
-    .then(response => {
-      return response.json()
-    })
-    .then(data => {
-      dispatch(setData(data));
-    })
+    return services.fetchGroupWeather(payload)
+           .then(data => {
+             dispatch(setData(data))
+           })
   };
 };
 
 // API call to get forecast data for the selected city
 export const getForecastByCityID = (payload) => {
   return(dispatch, getState) => {
-    fetch(`http://api.openweathermap.org/data/2.5/forecast?id=${payload}&units=imperial&APPID=23aa34fce77d5b7782d01b0917e6d2b6`)
-    .then(response => {
-      return response.json()
-    })
-    .then(data => {
-      dispatch(setCity(data))
-    })
+    return services.fetchCityForecast(payload)
+           .then(data => {
+             dispatch(setCity(data))
+           })
   };
 };
