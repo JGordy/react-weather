ce3de1afff61dc89ff4925191c20b70ae6f04123
Joseph Gordy <jgordy2424@gmail.com>
2018-02-02 11:12:20 -0500

    Changed the hardcoded cities to show better weather diversity

--- src/constants/places.js
+++ src/constants/places.js
@@ -1,7 +1,7 @@
 const PLACES = [
   { id: '4180439', name: 'Atlanta, GA', zip: '30350' },
-  { id: '4830796', name: 'Auburn, AL', zip: '36830' },
-  { id: '4059102', name: 'Dothan, AL', zip: '36301' },
+  { id: '2193733', name: 'Auckland, NZ', zip: '' },
+  { id: '264371', name: 'Athens, GR', zip: '' },
   { id: '4221552', name: 'Savannah, GA', zip: '31302' },
   { id: '4930956', name: 'Boston, MA', zip: '02215' },
   { id: '184745', name: 'Nairobi, KE', zip: ''}
